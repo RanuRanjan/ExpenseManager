9bfa35890ff8332c20d8aeeb2d3998dc84fb9f24
RanuRanjan <ranuranjan25@gmail.com>
2022-01-13 01:20:25 +0530

    multiple date and prps

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import './App.css';
 import ExpenseItem from './components/Expense/ExpenseItem';
 
 function App() {
+    const expense=[
+      {
+        id:'e1',
+        title:"school fee",
+        price:250,
+        date:new Date(2022,5,12)
+      }
+    ]
+
   return (
     <>
-         <ExpenseItem></ExpenseItem>
+         <ExpenseItem date={expense[0].date} title={expense[0].title} price={expense[0].price}></ExpenseItem>
     </>
   );
 }
--- src/components/Expense/ExpenseItem.jsx
+++ src/components/Expense/ExpenseItem.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import "./Expense.css"
+import ExpenseDate from './ExpenseDate'
 
-export default function ExpenseItem() {
+
+export default function ExpenseItem({date,title,price}) {
+    const DATE_OPTIONS = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
+    const Newdate= date.toLocaleDateString('en-US', DATE_OPTIONS)
     return (
         <div className='expense-item'>
-            <div>Jan 12th 2022</div>
+            <div>{Newdate}</div>
             <div className='expense-item__description'>
-                <h2>Car Insurance</h2>
-                <div className='expense-item__price'>$200</div>
+                <h2>{title}</h2>
+                <div className='expense-item__price'>${price}</div>
             </div>
         </div>
     )
